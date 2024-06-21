9b7938ffa6e0b7f35f85fa010bcf23bb45ecd72e
Shashwat <shashwat12028@gmail.com>
2024-06-21 14:21:02 +0530

    UI fixed

--- app/components/Companies/Companies.tsx
+++ app/components/Companies/Companies.tsx
@@ -1,6 +1,8 @@
 "use client";
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; // Import slick-carousel css
+import "slick-carousel/slick/slick-theme.css"; // Import slick-carousel theme css
 
 // IMAGES DATA FOR CAROUSEL
 interface Data {
@@ -29,66 +31,66 @@ const data: Data[] = [
 ];
 
 // CAROUSEL SETTINGS
-export default class MultipleItems extends Component {
-  render() {
-    const settings = {
-      dots: false,
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      arrows: false,
-      autoplay: true,
-      speed: 2000,
-      autoplaySpeed: 2000,
-      cssEase: "linear",
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false,
-          },
+const MultipleItems: React.FC = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
         },
-        {
-          breakpoint: 700,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false,
-          },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
         },
-        {
-          breakpoint: 500,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false,
-          },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
         },
-      ],
-    };
+      },
+    ],
+  };
 
-    return (
-      <div className="text-center bg-lightpink">
-        <div className="mx-auto max-w-2xl py-16 px-4s sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="text-lg my-10 text-lightgrey">
-            Trusted by companies of all sizes
-          </h2>
-          <div>
-            <Slider {...settings}>
-              {data.map((item, i) => (
-                <div key={i}>
-                  <img src={item.imgSrc} alt={item.imgSrc} />
-                </div>
-              ))}
-            </Slider>
-          </div>
+  return (
+    <div className="text-center bg-lightpink">
+      <div className="mx-auto max-w-2xl py-16 px-4s sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-lg my-10 text-lightgrey">
+          Trusted by companies of all sizes
+        </h2>
+        <div>
+          <Slider {...settings}>
+            {data.map((item, i) => (
+              <div key={i}>
+                <img src={item.imgSrc} alt={item.imgSrc} />
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default MultipleItems;
